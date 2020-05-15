f990a62b521dd01e49d0238d4f1ee5148832d0d5
adwin nugroho <adwinnugroho16@gmail.com>
2020-05-16 00:06:32 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import speech_recognition as sr
+from time import ctime
+r = sr.Recognizer()
+
+def record_audio():
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print("Sorry, I didn't get that")
+        except sr.RequestError:
+            print("Sorry, my speech service is down")
+        return voice_data
+
+def respond(voice_data):
+    if "what is your name" in voice_data:
+        print("My name is Didin") 
+    if "what time is it" in voice_data:
+        print(ctime())
+
+
+print("How can I help you?")
+voice_data = record_audio()
+respond(voice_data)
